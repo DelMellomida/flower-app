cd79af3f57eac47caed74f01923ec7d17bff23a1
Jhondel Mellomida <160392395+DelMellomida@users.noreply.github.com>
2024-07-15 05:57:38 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   const headingText = mood === 'neutral' ? "Bat kasi sa tuesday pa? :<": "I can't wait to see you!";
 
   return (
-    <><h1 className="text-center text-3xl font-bold mt-8">{headingText}</h1><p className="text-center text-xl mt-2">For now, here's a flower and a smile for you M.F :</p><Flower /><Smiley mood={mood} /><div className="button-box mt-4">
+    <><h1 className="text-center text-3xl font-bold mt-8">{headingText}</h1><p className="text-center text-xl mt-2">For now, here's a flower and a smile for you Margaret F. :</p><Flower /><Smiley mood={mood} /><div className="button-box mt-4">
       <button onClick={toggleMood} className="px-4 py-2 bg-yellow-500 text-white rounded-md shadow-md hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-400">
         Toggle Mood
       </button>
